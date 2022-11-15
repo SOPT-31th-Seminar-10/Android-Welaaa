cedaec60f49276c371a163ad02da114eec0b9265
SunWoong <sunwoong1122@gmail.com>
2022-11-15 21:53:57 +0900

    feat: bottom navigation 기능 구현 #1

--- app/src/main/java/com/sopt/welaaa/presentation/MainActivity.kt
+++ app/src/main/java/com/sopt/welaaa/presentation/MainActivity.kt
@@ -1,9 +1,36 @@
 package com.sopt.welaaa.presentation
 
+import android.os.Bundle
 import com.sopt.welaaa.R
 import com.sopt.welaaa.databinding.ActivityMainBinding
+import com.sopt.welaaa.presentation.home.view.HomeFragment
+import com.sopt.welaaa.presentation.search.view.SearchFragment
+import com.sopt.welaaa.presentation.setting.view.SettingFragment
+import com.sopt.welaaa.presentation.shelf.view.ShelfFragment
 import com.sopt.welaaa.util.binding.BindingActivity
+import com.sopt.welaaa.util.extension.replace
 
 class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
+        binding.bnvMain.itemIconTintList = null
+        setOnItemSelectedListener()
+        startTargetFragment(R.id.menu_home)
+    }
+
+    private fun setOnItemSelectedListener() {
+        binding.bnvMain.setOnItemSelectedListener { item ->
+            startTargetFragment(item.itemId)
+            true
+        }
+    }
+
+    private fun startTargetFragment(itemId: Int): Any = when (itemId) {
+        R.id.menu_home -> replace<HomeFragment>(R.id.fcv_main)
+        R.id.menu_search -> replace<SearchFragment>(R.id.fcv_main)
+        R.id.menu_shelf -> replace<ShelfFragment>(R.id.fcv_main)
+        R.id.menu_setting -> replace<SettingFragment>(R.id.fcv_main)
+        else -> IllegalArgumentException("Not found error.")
+    }
 }
