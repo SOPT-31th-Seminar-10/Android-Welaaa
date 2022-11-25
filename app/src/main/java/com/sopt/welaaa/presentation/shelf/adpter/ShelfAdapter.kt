a3e5b5791757c619c31c7078eba6e36d3de20f5b
hajeong <happykhj67@naver.com>
2022-11-25 09:36:42 +0900

    fix: shelf view 수정

---
+++ app/src/main/java/com/sopt/welaaa/presentation/shelf/adpter/ShelfAdapter.kt
@@ -0,0 +1,40 @@
+package com.sopt.welaaa.presentation.shelf.adpter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.sopt.welaaa.data.model.ResponseShelfDTO
+import com.sopt.welaaa.databinding.ItemBodyShelfBinding
+import com.sopt.welaaa.util.ItemDiffCallback
+
+class ShelfAdapter : ListAdapter<ResponseShelfDTO.Book,ShelfAdapter.ShelfViewHolder>(
+    ItemDiffCallback<ResponseShelfDTO.Book>(
+        onItemsTheSame = { oldItem, newItem -> oldItem == newItem },
+        onContentsTheSame = { oldItem, newItem -> oldItem == newItem }
+    )
+) {
+    private lateinit var inflater: LayoutInflater
+
+    override fun onCreateViewHolder(parent:ViewGroup, viewType:Int): ShelfViewHolder {
+        if (!::inflater.isInitialized) {
+            inflater = LayoutInflater.from(parent.context)
+        }
+        return ShelfViewHolder(ItemBodyShelfBinding.inflate(inflater, parent, false))
+    }
+    override fun onBindViewHolder(holder: ShelfViewHolder, position: Int) {
+        holder.onBind(getItem(position))
+    }
+
+    class ShelfViewHolder(private val binding: ItemBodyShelfBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun onBind(book: ResponseShelfDTO.Book) {
+            Glide.with(this.binding.root)
+                .load(book.image)
+                .into(binding.ivShelfBook)
+            binding.tvBookTitle.text = book.title
+            binding.tvBookAuthor.text = book.author
+        }
+    }
+}
--- app/src/main/java/com/sopt/welaaa/presentation/shelf/view/ShelfFragment.kt
+++ app/src/main/java/com/sopt/welaaa/presentation/shelf/view/ShelfFragment.kt
@@ -1,8 +1,61 @@
 package com.sopt.welaaa.presentation.shelf.view
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.sopt.welaaa.R
 import com.sopt.welaaa.databinding.FragmentShelfBinding
+import com.sopt.welaaa.presentation.shelf.adpter.ShelfAdapter
+import com.sopt.welaaa.presentation.shelf.viewmodel.ShelfViewModel
 import com.sopt.welaaa.util.binding.BindingFragment
 
 class ShelfFragment : BindingFragment<FragmentShelfBinding>(R.layout.fragment_shelf) {
+    class SearchFragment : Fragment() {
+        private var _binding: FragmentShelfBinding? = null
+        private val binding: FragmentShelfBinding
+            get() = _binding!!
+
+        private val shelfViewModel by viewModels<ShelfViewModel>()
+        override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+            _binding = FragmentShelfBinding.inflate(inflater, container, false)
+            return binding.root
+        }
+
+        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+            super.onViewCreated(view, savedInstanceState)
+
+            binding.lifecycleOwner = viewLifecycleOwner
+            initAdapter()
+            initView()
+            setObserver()
+        }
+
+        private fun initView() {
+            shelfViewModel.getShelf()
+        }
+
+        private var shelfAdapter: ShelfAdapter = ShelfAdapter()
+
+        private fun initAdapter() {
+            binding.rvShelf.adapter = shelfAdapter
+        }
+
+        private fun setObserver() {
+            shelfViewModel.shelfResult.observe(viewLifecycleOwner) {
+                shelfAdapter.submitList(it.data.toMutableList())
+            }
+        }
+
+        override fun onDestroyView() {
+            super.onDestroyView()
+            _binding = null
+        }
+    }
 }
